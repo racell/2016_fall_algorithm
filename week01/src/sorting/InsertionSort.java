0c3fe01b0a2ce2d3a9067b1bb1cad9ea2a7edd8f
racell <fjunkimsj@gmail.com>
2016-09-16 21:14:42 +0900

    Fix insertionsort with arraylist

--- week01/src/sorting/InsertionSort.java
+++ week01/src/sorting/InsertionSort.java
@@ -6,29 +6,26 @@ import java.util.ArrayList;
  * Created by SeongJung on 2016-09-16.
  */
 public class InsertionSort {
-    private int[] array;
-    private int length;
+    private ArrayList<Integer> array;
 
-    public InsertionSort(int[] array) {
+    public InsertionSort(ArrayList<Integer> array) {
         this.array = array;
     }
 
     public void doInsertionSort() {
-        for (int i = 1; i < array.length; i++) {
-            int key = this.array[i];
+        for (int i = 1; i < array.size(); i++) {
+            int key = this.array.get(i);
             int j = i - 1;
 
-            while (j >= 0 && this.array[j] > key) {
-                this.array[j+1] = this.array[j];
+            while (j >= 0 && this.array.get(j) > key) {
+                this.array.set(j+1, this.array.get(j));
                 j = j - 1;
             }
-            this.array[j+1] = key;
+            this.array.set(j+1, key);
         }
     }
 
     public void printResult() {
-        for (int i = 0; i < this.array.length; i++) {
-            System.out.println(this.array[i]);
-        }
+        System.out.println(this.array.toString());
     }
 }
